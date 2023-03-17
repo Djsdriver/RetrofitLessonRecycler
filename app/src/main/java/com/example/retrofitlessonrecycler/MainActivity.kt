c28933a1b18f03cced97e6f7b903681a9e8e8d2b
Djsdriver <alexei.gra4ev@gmail.com>
2023-03-17 12:55:42 +0300

    Добавил функцию поиска

--- app/src/main/java/com/example/retrofitlessonrecycler/MainActivity.kt
+++ app/src/main/java/com/example/retrofitlessonrecycler/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.retrofitlessonrecycler
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SearchView
+import android.widget.SearchView.OnQueryTextListener
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.retrofitlessonrecycler.retrofit.ProductApi
@@ -11,18 +13,23 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var  recycler: RecyclerView
+    lateinit var recycler: RecyclerView
+    lateinit var sv: SearchView
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val interceptor= HttpLoggingInterceptor()
+        val interceptor = HttpLoggingInterceptor()
 
         interceptor.level = HttpLoggingInterceptor.Level.BODY
 
@@ -35,21 +42,47 @@ class MainActivity : AppCompatActivity() {
             .addConverterFactory(GsonConverterFactory.create()).build()
 
 
-        recycler=findViewById(R.id.recycler)
+        recycler = findViewById(R.id.recycler)
+        sv = findViewById(R.id.sv)
 
-        recycler.layoutManager=LinearLayoutManager(this)
+        recycler.layoutManager = LinearLayoutManager(this)
         val productApi = retrofit.create(ProductApi::class.java)
 
-        CoroutineScope(Dispatchers.IO).launch {
-            val list=productApi.getAllProducts()
-            runOnUiThread {
-                recycler.adapter=ProductAdapter(list.products)
+
+
+        sv.setOnQueryTextListener(object : OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                return true
             }
-        }
 
+            override fun onQueryTextChange(p0: String?): Boolean {
+                // val list=productApi.getAllProducts()
+                // val query=productApi.getProductsByName(p0.toString())
+                // recycler.adapter=ProductAdapter(query.products)
+                p0?.let {
+                    productApi.getProductsByName(it)
+                        .enqueue(object : Callback<Products> {
+                            override fun onResponse(
+                                call: Call<Products>,
+                                response: Response<Products>
+                            ) {
+                                recycler.adapter =
+                                    response.body()?.let { ProductAdapter(it.products) }
 
+                            }
 
+                            override fun onFailure(call: Call<Products>, t: Throwable) {
 
+                            }
+
+                        })
+                }
+
+                return true
+
+            }
+
+        })
 
 
     }
--- app/src/main/java/com/example/retrofitlessonrecycler/retrofit/ProductApi.kt
+++ app/src/main/java/com/example/retrofitlessonrecycler/retrofit/ProductApi.kt
@@ -2,10 +2,16 @@ package com.example.retrofitlessonrecycler.retrofit
 
 import com.example.retrofitlessonrecycler.Product
 import com.example.retrofitlessonrecycler.Products
+import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface ProductApi {
 
     @GET("products")
     suspend fun getAllProducts(): Products
+
+    @GET("products/search")
+    fun getProductsByName(@Query("q") name: String): Call<Products>
 }
